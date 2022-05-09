12dd9c464dc814711aff27f5aa2c68e6731592ae
MijnendonckxYara <yara.mijnendonckx@student.uhasselt.be>
2022-05-09 14:39:33 +0200

    data storage

--- app/src/main/java/com/example/dnd_list_2/ListFragment.kt
+++ app/src/main/java/com/example/dnd_list_2/ListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dnd_list_2
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,7 +17,6 @@ class ListFragment: Fragment(R.layout.fragment_list1) {
     private lateinit var main: MainActivity
     private lateinit var adapter: ListAdapter
 
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,19 +30,26 @@ class ListFragment: Fragment(R.layout.fragment_list1) {
 
         binding.rvwList.layoutManager = LinearLayoutManager(this.context)
 
-
+        val sharedPref = activity?.getSharedPreferences(
+            "Dnd_list_appdev", Context.MODE_PRIVATE)
 
         binding.btnAddList.setOnClickListener {
             val newListTitle = binding.edtList.text.toString()
             List_example.add(List(newListTitle))
             adapter.notifyItemInserted(List_example.size - 1)
+
+            if (sharedPref != null) {
+                with (sharedPref.edit()) {
+                    putString("title", newListTitle)
+                    apply()
+                }
+            }
+
             binding.edtList.text.clear()
             binding.edtList.clearFocus()
             main.hideKeyboard(it)
         }
         return binding.root
-
-
 
     }
 
@@ -50,12 +57,14 @@ class ListFragment: Fragment(R.layout.fragment_list1) {
         List_example.clear()
         adapter.notifyDataSetChanged()
     }
+
     fun clearLatestItem(){
         if(List_example.size>=1){
             List_example.removeAt(List_example.size-1)
             adapter.notifyItemRemoved(List_example.size-1)
         }
     }
+
     fun resetItems(){
         List_example.clear()
         List_example.addAll(sampleList())
