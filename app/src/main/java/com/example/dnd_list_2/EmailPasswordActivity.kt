a2a428b000223edd68766acc77850d4b503a9cf4
MijnendonckxYara <yara.mijnendonckx@student.uhasselt.be>
2022-08-16 23:45:30 +0200

    login verder uitgewerkt

--- app/src/main/java/com/example/dnd_list_2/EmailPasswordActivity.kt
+++ app/src/main/java/com/example/dnd_list_2/EmailPasswordActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dnd_list_2
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -27,17 +28,15 @@ class EmailPasswordActivity : Activity() {
         }
     }
 
-    private fun createAccount(email: String, password: String) {
+    fun createAccount(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
                     updateUI(null)
@@ -45,17 +44,15 @@ class EmailPasswordActivity : Activity() {
             }
     }
 
-    private fun signIn(email: String, password: String) {
+    fun signIn(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
                     updateUI(null)
--- app/src/main/java/com/example/dnd_list_2/LoginFragment.kt
+++ app/src/main/java/com/example/dnd_list_2/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.dnd_list_2.databinding.FragmentLoginBinding
 
@@ -11,6 +13,12 @@ import com.example.dnd_list_2.databinding.FragmentLoginBinding
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding : FragmentLoginBinding
+    //private val main: MainActivity = MainActivity()
+    private val emailPasswordActivity = EmailPasswordActivity()
+
+    private lateinit var mail : EditText
+    private lateinit var password : EditText
+    private lateinit var login : Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,8 +28,19 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         binding = FragmentLoginBinding.inflate(layoutInflater)
 
 
+        mail = binding.txtTheMail
+        password = binding.txtThePassword
+        login = binding.btnLogin
 
+        login.setOnClickListener {
+            //emailPasswordActivity.createAccount(mail.toString(), password.toString())
+            //emailPasswordActivity.signIn(mail.toString(), password.toString())
 
+            //main.hideKeyboard(it)
+
+            val startFragment = StartFragment()
+            (activity as MainActivity).switchTo(startFragment)
+        }
 
         return binding.root
     }
