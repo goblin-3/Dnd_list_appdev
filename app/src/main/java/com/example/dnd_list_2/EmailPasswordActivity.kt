59dbf453c5a9d5a1ff5fe263f1ad3a4b32d1c147
MijnendonckxYara <yara.mijnendonckx@student.uhasselt.be>
2022-08-17 11:42:25 +0200

    login en signup werken

--- app/src/main/java/com/example/dnd_list_2/EmailPasswordActivity.kt
+++ app/src/main/java/com/example/dnd_list_2/EmailPasswordActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -64,13 +67,9 @@ class EmailPasswordActivity : Activity() {
         Firebase.auth.signOut()
     }
 
-    private fun updateUI(user: FirebaseUser?) {
+    private fun updateUI(user: FirebaseUser?) {}
 
-    }
-
-    private fun reload() {
-
-    }
+    private fun reload() {    }
 
     companion object {
         private const val TAG = "EmailPassword"
--- app/src/main/java/com/example/dnd_list_2/LoginFragment.kt
+++ app/src/main/java/com/example/dnd_list_2/LoginFragment.kt
@@ -5,22 +5,16 @@ import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.dnd_list_2.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding : FragmentLoginBinding
-    private val emailPasswordActivity = EmailPasswordActivity()
-
-    private lateinit var mail : EditText
-    lateinit var sMail : String
-    private lateinit var password : EditText
-    lateinit var sPassword : String
-    private lateinit var login : Button
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,25 +23,45 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     ): View? {
         binding = FragmentLoginBinding.inflate(layoutInflater)
 
+        val mail = binding.txtTheMail
+        val password = binding.txtThePassword
 
-        mail = binding.txtTheMail
-        password = binding.txtThePassword
-        login = binding.btnLogin
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        login.setOnClickListener {
-            sMail = mail.text.toString()
-            sPassword = password.text.toString()
+        binding.btnLogin.setOnClickListener {
+            val sMail = mail.text.toString()
+            val sPassword = password.text.toString()
 
             if (!isEmailValid(sMail)){
                 binding.txtTheMail.setError("Enter correct mail")
             } else if (sPassword.length < 7) {
                 binding.txtThePassword.setError("Password must be 7 characters")
             } else {
-                //emailPasswordActivity.createAccount(mail.toString(), password.toString())
-                //emailPasswordActivity.signIn(mail.toString(), password.toString())
+                firebaseAuth.signInWithEmailAndPassword(sMail, sPassword).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val startFragment = StartFragment()
+                        (activity as MainActivity).switchTo(startFragment)
+                    } else {
+                        Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
-                val startFragment = StartFragment()
-                (activity as MainActivity).switchTo(startFragment)
+        binding.txtSignUp.setOnClickListener{
+            val sMail = mail.text.toString()
+            val sPassword = password.text.toString()
+
+            if (!isEmailValid(sMail)){
+                binding.txtTheMail.setError("Enter correct mail")
+            } else if (sPassword.length < 7) {
+                binding.txtThePassword.setError("Password must be 7 characters")
+            } else {
+                firebaseAuth.createUserWithEmailAndPassword(sMail, sPassword).addOnCompleteListener {
+                    if (!it.isSuccessful) {
+                        Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
 
